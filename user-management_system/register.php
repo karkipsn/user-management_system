36c57ff152fa9c5bdf89d0814334bc68963fed83
karkipsn <20148471+karkipsn@users.noreply.github.com>
2018-05-28 15:30:12 +0545

    Update register.php

--- user-management_system/register.php
+++ user-management_system/register.php
@@ -67,38 +67,6 @@ if(isset($_POST['register_btn'])){
 }
 ?>
 
-<?php
-
-
-if(isset($_POST['btn-signup']))
-{
-	
-		try
-		{
-			$stmt = $user->runQuery("SELECT user_name, user_email FROM users WHERE user_name=:uname OR user_email=:umail");
-			$stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
-			$row=$stmt->fetch(PDO::FETCH_ASSOC);
-				
-			if($row['user_name']==$uname) {
-				$error[] = "sorry username already taken !";
-			}
-			else if($row['user_email']==$umail) {
-				$error[] = "sorry email id already taken !";
-			}
-			else
-			{
-				if($user->register($uname,$umail,$upass)){	
-					$user->redirect('index.php?joined');
-				}
-			}
-		}
-		catch(PDOException $e)
-		{
-			echo $e->getMessage();
-		}
-	}	
-
-?>
 
 <!DOCTYPE html>
 <html>
